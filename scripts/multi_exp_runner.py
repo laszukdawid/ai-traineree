feed6abdeb5265e3ed5a55106ca23cf14b16a5eb
Dawid Laszuk <laszukdawid@gmail.com>
2021-01-25 11:43:44 -0800

    Add script to run multi exp on same seeds with diff params

---
+++ scripts/multi_exp_runner.py
@@ -0,0 +1,31 @@
+import torch
+
+from ai_traineree.agents.dqn import DQNAgent as Agent
+from ai_traineree.env_runner import EnvRunner
+from ai_traineree.loggers import TensorboardLogger
+from ai_traineree.tasks import GymTask
+from typing import Any, Dict
+from pprint import pprint
+
+
+config_default = {'hidden_layers': (50, 50)}
+config_updates = [{'n_steps': n} for n in range(1, 11)]
+
+task = GymTask("CartPole-v1")
+seeds = [32167, 1, 999, 2833700, 13]
+
+for idx, config_update in enumerate(config_updates):
+    config: Dict[str, Any] = config_default.copy()
+    config.update(config_update)
+
+    for seed in seeds:
+        config['seed'] = seed
+        pprint(config)
+        torch.manual_seed(config['seed'])
+        agent = Agent(task.state_size, task.action_size, **config)
+
+        data_logger = TensorboardLogger(log_dir=f'runs/MultiExp-{task.name}-i{idx}-s{seed}')
+        env_runner = EnvRunner(task, agent, data_logger=data_logger)
+        env_runner.seed(seed)
+        env_runner.run(reward_goal=99999, max_episodes=500, eps_decay=0.95, force_new=True)
+        data_logger.close()
