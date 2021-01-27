b70521a536363c2589ffbb2a5997050225dc08bf
Dawid Laszuk <laszukdawid@gmail.com>
2021-01-26 21:59:13 -0800

    IQL update to match Nx DQN

--- ai_traineree/multi_agents/iql.py
+++ ai_traineree/multi_agents/iql.py
@@ -1,21 +1,22 @@
 import torch
 
 from ai_traineree import DEVICE
-from ai_traineree.buffers import ReplayBuffer
 from ai_traineree.agents.dqn import DQNAgent
 from ai_traineree.loggers import DataLogger
 from ai_traineree.types import ActionType, MultiAgentType, StateType
-from typing import Dict, List, Optional
+from typing import Dict
 
 
-class IQLAgent(MultiAgentType):
+class IQLAgents(MultiAgentType):
 
     name = "IQL"
 
     def __init__(self, state_size: int, action_size: int, num_agents: int, **kwargs):
-        """Initiation of the Multi Agent DDPG.
+        """Independent Q-Learning
 
-        All keywords are also passed to DDPG agents.
+        A set of independent Q-Learning agents (DQN implementation) that are organized
+        to work as an `Multi Agent` agent. These agents have defaults as per DQNAgent class.
+        All keyword paramters are passed to each agent.
 
         Parameters:
             state_size (int): Dimensionality of the state.
@@ -27,7 +28,6 @@ class IQLAgent(MultiAgentType):
             noise_scale (float): Default: 1.0. Noise amplitude.
             noise_sigma (float): Default: 0.5. Noise variance.
             actor_lr (float): Default: 0.001. Learning rate for actor network.
-            critic_lr (float): Default: 0.001. Learning rate for critic network.
             gamma (float): Default: 0.99. Discount value
             tau (float): Default: 0.02. Soft copy value.
             gradient_clip (optional float): Max norm for learning gradient. If None then no clip.
@@ -39,39 +39,26 @@ class IQLAgent(MultiAgentType):
 
         """
 
-        self.device = self._register_param(kwargs, "device", DEVICE)
         self.state_size: int = state_size
         self.action_size = action_size
         self.num_agents = num_agents
         self.agent_names = kwargs.get("agent_names", map(str, range(self.num_agents)))
 
-        hidden_layers = self._register_param(kwargs, 'hidden_layers', (256, 128))
-        noise_scale = float(self._register_param(kwargs, 'noise_scale', 0.5))
-        noise_sigma = float(self._register_param(kwargs, 'noise_sigma', 1.0))
-        actor_lr = float(self._register_param(kwargs, 'actor_lr', 1e-3))
-        critic_lr = float(self._register_param(kwargs, 'critic_lr', 1e-3))
+        kwargs['device'] = self._register_param(kwargs, "device", DEVICE)
+        kwargs['hidden_layers'] = self._register_param(kwargs, 'hidden_layers', (64, 64))
+        kwargs['gamma'] = float(self._register_param(kwargs, 'gamma', 0.99))
+        kwargs['tau'] = float(self._register_param(kwargs, 'tau', 0.002))
+        kwargs['gradient_clip'] = self._register_param(kwargs, 'gradient_clip')
+        kwargs['batch_size'] = int(self._register_param(kwargs, 'batch_size', 64))
+        kwargs['buffer_size'] = int(self._register_param(kwargs, 'buffer_size', int(1e6)))
+        kwargs['warm_up'] = int(self._register_param(kwargs, 'warm_up', 0))
+        kwargs['update_freq'] = int(self._register_param(kwargs, 'update_freq', 1))
+        kwargs['number_updates'] = int(self._register_param(kwargs, 'number_updates', 1))
 
         self.agents: Dict[str, DQNAgent] = {
-            agent_name: DQNAgent(
-                state_size, action_size, hidden_layers=hidden_layers,
-                name=agent_name,
-                actor_lr=actor_lr, critic_lr=critic_lr,
-                noise_scale=noise_scale, noise_sigma=noise_sigma,
-                **kwargs,
+            agent_name: DQNAgent(state_size, action_size, name=agent_name, **kwargs,
             ) for agent_name in self.agent_names
         }
-
-        self.gamma: float = float(self._register_param(kwargs, 'gamma', 0.99))
-        self.tau: float = float(self._register_param(kwargs, 'tau', 0.002))
-        self.gradient_clip: Optional[float] = self._register_param(kwargs, 'gradient_clip')
-
-        self.batch_size: int = int(self._register_param(kwargs, 'batch_size', 64))
-        self.buffer_size = int(self._register_param(kwargs, 'buffer_size', int(1e6)))
-        self.buffer = ReplayBuffer(self.batch_size, self.buffer_size)
-
-        self.warm_up: int = int(self._register_param(kwargs, 'warm_up', 0))
-        self.update_freq: int = int(self._register_param(kwargs, 'update_freq', 1))
-        self.number_updates: int = int(self._register_param(kwargs, 'number_updates', 1))
 
         self.reset()
 
@@ -107,7 +94,7 @@ class IQLAgent(MultiAgentType):
         return self.agents[agent_name].step(state, action, reward, next_state, done)
 
     @torch.no_grad()
-    def act(self, agent_name: str, state: List[StateType], noise: float=0.0) -> ActionType:
+    def act(self, agent_name: str, state: StateType, noise: float=0.0) -> ActionType:
         return self.agents[agent_name].act(state, noise)
 
     def log_metrics(self, data_logger: DataLogger, step: int, full_log: bool=False):
@@ -128,9 +115,6 @@ class IQLAgent(MultiAgentType):
         self.__dict__.update(**self._config)
         for agent_id, agent in enumerate(self.agents):
             agent.actor.load_state_dict(agent_state[f'actor_{agent_id}'])
-            agent.critic.load_state_dict(agent_state[f'critic_{agent_id}'])
-            agent.target_actor.load_state_dict(agent_state[f'target_actor_{agent_id}'])
-            agent.target_critic.load_state_dict(agent_state[f'target_critic_{agent_id}'])
             agent._config = agent_state[f'config_{agent_id}'].get(f'config_{agent_id}', {})
             agent.__dict__.update(**agent._config)
     
