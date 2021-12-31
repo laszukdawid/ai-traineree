db0d0c22ccb026138612c907997bdc3b5c3554d9
Dawid Laszuk <11457963+laszukdawid@users.noreply.github.com>
2021-12-31 10:58:07 -0800

    Fix SAC by fixing MultivariateNormalSimple policy (#29)
    * This potentially runs (slightly different runs!)
    
    * Polish policy and update example to faster
    
    * Update tests to fixed SAC and MultiNormalSimple
    
    * Fixing package sorting

--- ai_traineree/policies.py
+++ ai_traineree/policies.py
@@ -1,10 +1,8 @@
-import math
 from functools import lru_cache
 from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.distributions import Beta, Dirichlet, MultivariateNormal, Normal
 from torch.distributions.distribution import Distribution
 
@@ -21,23 +19,22 @@ class MultivariateGaussianPolicySimple(PolicyType):
     """
     Multivariate Gaussian (Normal) Policy.
 
-    Simplicity of this class, compared to `MultivariateGaussianPolicy`, is due to
-    the assumption that the covariance is sample independent and it is a trainable
-    parameter.
+    Simplicity of this class, compared to `MultivariateGaussianPolicy`, is in
+    the assumption that the covariance is diagonal, sample independent and
+    is treated a trainable parameter.
     """
 
     param_dim = 1
 
     def __init__(
-        self, size: int, std_init: float = 1.0, std_min: float = 0.1, std_max: float = 3.0, device=None, **kwargs
+        self, size: int, std_init: float = 0.5, std_min: float = 0.0001, std_max: float = 2.0, device=None, **kwargs
     ):
         """
         Parameters:
-            size: Size of the observation.
-            batch_size: Expected size of batch. Helps in memory assignments.
-            std_init: (default 2) Initial value for covariance's diagonal. All values start the same.
-            std_min: Minimum value for standard deviation.
-            std_max: Maximum value for standard deviation.
+            size (int): Size of the observation.
+            std_init: Initial value for covariance's diagonal. All values start the same. Default: 0.5.
+            std_min: Minimum value for standard deviation. Default: 0.0001.
+            std_max: Maximum value for standard deviation. Default: 2.
             device: Device where to allocate memory. CPU or CUDA.
 
         """
@@ -59,31 +56,40 @@ class MultivariateGaussianPolicySimple(PolicyType):
     def diag_idx(batch_size: int, size: int, device):
         return torch.arange(size, device=device).repeat((batch_size, 1, 1))
 
-    def forward(self, x) -> Distribution:
-        """Returns distribution"""
+    def forward(self, x, deterministic: bool = False) -> Distribution:
+        """Samples from distribution.
+
+        Parameters:
+            x (tensor): Uses a location (mu) for the distrubition.
+            deterministic (bool): Whether to sample from distribution, or use estimates.
+                Default: False, i.e. it'll sample distribution.
+
+        """
+        if deterministic:
+            return x.view(-1, 1)
+
+        x = x.float()  # Need to convert as dist doesn't work on int
         self.std.data = torch.clamp(self.std, self.std_min, self.std_max)
+
         if self.size == 1:
             return self.dist(x.view(-1, 1), scale=self.std.view(-1, 1))
 
-        # Distinction here is primarily performance optimization (though it isn't too optimal)
         batch_size = x.shape[0]
-        if len(x.shape) == 1 or x.shape[0] == 1:
-            new_shape = (1, self.size, 1)
-            idx = torch.arange(self.size, device=x.device).view(new_shape)
-            std = self._empty_std(batch_size, self.size, x.device).scatter(-1, idx, self.std.repeat(new_shape))
-        else:
-            std = self.std.repeat((batch_size, 1, 1))
-            std = self._empty_std(batch_size, self.size, x.device).scatter(
-                1, self.diag_idx(batch_size, self.size, x.device), std
-            )
-        return self.dist(x, scale_tril=std)
+        new_shape = (batch_size, 1, 1)
+        idx = self.diag_idx(batch_size, self.size, device=x.device)
+        std_shell = self._empty_std(batch_size, self.size, x.device)
+        std_vals = self.std.repeat(new_shape)
+        std = std_shell.scatter(1, idx, std_vals)
+        self._last_samples = {"mu": x, "std": self.std}
+        self._last_dist = self.dist(x, scale_tril=std)
+        return self._last_dist.rsample()
 
     def act(self, x):
-        return x
+        return self.forward(x, deterministic=True)
 
-    @staticmethod
-    def log_prob(dist, samples) -> torch.Tensor:
-        return dist.log_prob(samples)
+    def log_prob(self, samples) -> torch.Tensor:
+        assert self._last_dist is not None, "Need to execute `forward` on data first"
+        return self._last_dist.log_prob(samples)
 
 
 class MultivariateGaussianPolicy(PolicyType):
--- examples/lunar_lander_sac.py
+++ examples/lunar_lander_sac.py
@@ -1,3 +1,4 @@
+# *Note* that this example isn't optimial but it should work.
 import datetime
 
 import pylab as plt
@@ -9,39 +10,41 @@ from ai_traineree.runners.env_runner import EnvRunner
 from ai_traineree.tasks import GymTask
 from ai_traineree.types import TaskType
 
-
-def reward_transform(reward):
-    """Cap reward to [-1, 1]"""
-    return max(-1, min(reward, 1))
-
-
-seed = 32167
+seed = 321671
 torch.manual_seed(seed)
 
 
 env_name = "LunarLanderContinuous-v2"
-task: TaskType = GymTask(env_name, seed=seed, reward_transform=reward_transform)
+task: TaskType = GymTask(env_name, seed=seed)
 config = {
-    "warm_up": 400,
+    "warm_up": 5000,
     "device": "cpu",
-    "batch_size": 60,
-    "update_freq": 2,
-    "number_updates": 1,
-    "hidden_layers": (100, 100),
-    "actor_lr": 5e-4,
-    "critic_lr": 5e-4,
-    "alpha_lr": 3e-5,
-    "tau": 0.02,
-    "alpha": 0.2,
+    "batch_size": 200,
+    "update_freq": 500,
+    "number_updates": 10,
+    "actor_number_updates": 5,
+    "critic_number_updates": 5,
+    "max_grad_norm_actor": 10,
+    "max_grad_norm_critic": 10,
+    "max_grad_norm_alpha": 3,
+    "hidden_layers": (200, 200),
+    "actor_lr": 1e-3,
+    "critic_lr": 3e-4,
+    "alpha_lr": 3e-4,
+    "tau": 0.005,
+    "alpha": 0.3,
     "seed": seed,
+    "simple_policy": True,
 }
 agent = SACAgent(task.obs_space, task.action_space, **config)
 
 log_dir = f"runs/{env_name}_{agent.model}-{datetime.datetime.now().isoformat()[:-7]}"
 data_logger = TensorboardLogger(log_dir=log_dir)
-env_runner = EnvRunner(task, agent, data_logger=data_logger, seed=seed)
-scores = env_runner.run(reward_goal=30, max_episodes=500, eps_end=0.01, eps_decay=0.95, force_new=True)
-env_runner.interact_episode(0, render=True)
+env_runner = EnvRunner(task, agent, data_logger=data_logger, seed=seed, debug_log=True)
+scores = env_runner.run(
+    reward_goal=30, max_episodes=2000, eps_end=0.01, eps_decay=0.95, force_new=True, checkpoint_every=None
+)
+env_runner.interact_episode(eps=0, render=True)
 data_logger.close()
 
 # plot the scores
