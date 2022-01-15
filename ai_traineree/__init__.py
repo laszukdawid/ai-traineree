61dcadb591d473add14d4aa2aa8364cd43dcba9a
Dawid Laszuk <laszukdawid@gmail.com>
2022-01-15 15:25:09 -0800

    Update github action to publish on version change

--- ai_traineree/__init__.py
+++ ai_traineree/__init__.py
@@ -3,8 +3,6 @@ import torch
 __version__ = "0.4.2"
 
 
-# This is expected to be safe, although in PyTorch 1.7 it comes as a warning,
-# if CUDA is not installed.
 try:
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 except Exception:
